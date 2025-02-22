ad2f18e6719443a315d4719ad08a15f940f60798
Zeppth <151110530+Zeppth@users.noreply.github.com>
2025-02-22 15:46:41 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { connServerDB } from "./fun.connServer.js";
-import { createServerDB } from "./fun.createServer";
+import { createServerDB } from "./fun.createServer.js";
 import { LowDB } from "./fun.lowdb.js";
 import color from "./fun.color.js"
 
